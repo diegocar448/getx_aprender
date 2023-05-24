4d58ea788f17f88ef7938d136b1c2296ee616d2c
Diego Cardoso <diegocar448@hotmail.com>
2023-05-24 16:45:23 -0300

    Starter 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const MyApp());
 }
 
-class HomePage extends StatelessWidget {
-  //const HomePage({super.key});
-  const HomePage({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  final textController = TextEditingController();
+  String definedValue = '';
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           // Valor
-          const Text("Valor definido"),
+          Text('Valor definido: $definedValue'),
           // Campo
-          TextField(),
+          TextField(
+            controller: textController,
+          ),
           // Botão
           ElevatedButton(
-            onPressed: () {},
-            child: Text('Confirmar'),
+            onPressed: () {
+              String value = textController.text;
+
+              setState(() {
+                definedValue = value;
+              });
+            },
+            child: const Text('Confirmar'),
           )
         ],
       ),
